774920332931dde3413b16ab6b312d5e5baf6a48
itsGozzy <gosdeng@gmail.com>
2017-01-28 00:17:02 +0000

    Added infinite random bear spawning + bear varieties.
    Bears now spawn infinitely, and there are two different type of zombie
    bears: "Hard Bears" and "Cute Bears" they have different speeds and
    different health.

---
+++ Dungeon Bears/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+	public GameObject[] enemies;
+	public Vector3 spawnStuff;
+	public float spawnWait;
+	public int startWait;
+	public float spawnMostWait;
+	public float spawnLeastWait;
+	public bool stopSpawning;
+	int randomEnemy;
+
+	void Start () {
+		StartCoroutine (Spawner());
+	
+	}
+
+	void Update () {
+		spawnWait = Random.Range (spawnLeastWait, spawnMostWait);
+	
+	}
+
+	IEnumerator Spawner()
+	{
+		yield return new WaitForSeconds (startWait);
+		while (!stopSpawning) 
+		{
+			randomEnemy = Random.Range (0, 2);
+
+			Vector3 spawnPos = new Vector3(Random.Range(-spawnStuff.x, spawnStuff.x), 1, Random.Range(-spawnStuff.z, spawnStuff.z));
+
+			Instantiate (enemies [randomEnemy], spawnPos + transform.TransformPoint (0, 0, 0), gameObject.transform.rotation);
+			yield return new WaitForSeconds (spawnWait);
+
+		}
+	}
+}
